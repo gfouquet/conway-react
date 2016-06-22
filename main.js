904f854eb15c0c4c614db32b100209a1a21bba15
gfouquet <gfouquet@henix.fr>
2016-06-22 13:06:00 +0200

    Added a logger middleware for the sake of it

--- main.js
+++ main.js
@@ -7,6 +7,17 @@ import conway from './reducers'
 
 const store = createStore(conway)
 
+const logger = (store) => (next) => (action) => {
+    console.group(`Dispatching action ${action.type}`)
+    console.log("Action: ", action)
+    console.log("Prev state: ", store.getState())
+    next(action)
+    console.log("Next state: ", store.getState())
+    console.groupEnd()
+}
+
+store.dispatch = logger(store)(store.dispatch)
+
 render(
     <Provider store={store}><App /></Provider>,
     document.getElementById('app')
